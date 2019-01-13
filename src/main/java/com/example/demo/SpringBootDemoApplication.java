020be724b2ef8dc1549dd403c2dce0578c1d1698
chenqd <chenqd@tydic.com>
2019-01-13 22:36:04 +0800

    修改controller 名称

--- src/main/java/com/example/demo/SpringBootDemoApplication.java
+++ src/main/java/com/example/demo/SpringBootDemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 默认扫描整个demo 包，所以不用写了
+//@ComponentScan(basePackages = {"com.example.demo"})
 @SpringBootApplication
 public class SpringBootDemoApplication {
 
--- src/main/java/com/example/demo/controllers/IndexController.java
+++ src/main/java/com/example/demo/controllers/IndexController.java
@@ -1,20 +1,16 @@
 package com.example.demo.controllers;
 
-import com.example.demo.beans.Book;
-import com.example.demo.mappers.BookMapper;
+
 import com.example.demo.services.IndexService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController /* 约等于@Controller + @ResponseBody 但是也有缺陷：不能返回jsp或者html*/
-@SpringBootApplication
-// 需要扫描整个demo 包
-@ComponentScan(basePackages = {"com.example.demo"})
-public class DemoApplication {
+
+
+public class IndexController {
 
     @Autowired
     IndexService indexService;
@@ -22,7 +18,5 @@ public class DemoApplication {
     String index(){
         return indexService.index();
     }
-    public static void main(String[] args){
-        SpringApplication.run(DemoApplication.class,args);
-    }
+
 }
